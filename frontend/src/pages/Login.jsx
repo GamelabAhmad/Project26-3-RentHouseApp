7a64f7a54ddb973f277a2e9b5c78fb5c740e7d03
aryaa25 <aryacksn02@gmail.com>
2024-06-08 20:38:00 +0700

    update tampilan register dan login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,69 +4,59 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function Login() {
   return (
-    <div
-      className="flex items-center justify-center min-h-screen bg-custom-gradient"
-      style={{
-        backgroundImage: `url('/img/house-landingpage-final.png')`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-      }}
-    >
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md md:max-w-lg relative">
+    <div className="flex items-center justify-center min-h-screen bg-white font-spartan">
+      <div className="relative bg-white p-8 rounded-lg w-full max-w-md md:max-w-lg">
         <NavLink
           className="absolute top-4 left-4 text-gray-700 hover:text-gray-900"
           to="/"
         >
           <AiOutlineArrowLeft size={24} />
         </NavLink>
-        <form className="space-y-6 font-spartan">
-          <h2 className="text-2xl font-bold text-slate-950 text-center font-spartan">
-            Login
-          </h2>
+        <div className="text-center mb-8">
+          <h2 className="text-2xl font-bold text-slate-950">Login</h2>
+          <p className="text-gray-700">Welcome, Please enter your details</p>
+        </div>
+        <form className="space-y-6">
           <div className="form-group">
             <label
               htmlFor="email"
-              className="block mb-2 text-gray-700 font-semibold font-spartan"
+              className="block mb-2 text-gray-700 font-semibold"
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              placeholder="Masukkan Email Anda"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 font-spartan"
+              placeholder="Email"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div className="form-group">
             <label
               htmlFor="password"
-              className="block mb-2 text-gray-700 font-semibold font-spartan"
+              className="block mb-2 text-gray-700 font-semibold"
             >
               Password
             </label>
             <input
               type="password"
               id="password"
-              placeholder="Masukkan Password Anda"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 font-spartan"
+              placeholder="Password"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 bg-yellow-400 text-white rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-orange-600 font-semibold font-spartan"
+            className="w-full py-2 bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold"
           >
             Login
           </button>
-          <p className="text-center text-gray-700 mt-4 font-spartan">
-            Atau login menggunakan akun Google
+          <p className="text-center text-gray-700 mt-4">
+            Don't have an account?{" "}
+            <NavLink to="/register" className="text-blue-500">
+              Register
+            </NavLink>
           </p>
-          <NavLink
-            className="block w-full py-2 text-center bg-yellow-400 text-white rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-orange-600 mt-2 font-spartan"
-            to="/register"
-          >
-            Daftar
-          </NavLink>
         </form>
       </div>
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,83 +4,73 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function Register() {
   return (
-    <div
-      className="flex items-center justify-center min-h-screen bg-custom-gradient font-spartan"
-      style={{
-        backgroundImage: `url('/img/house-landingpage-final.png')`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-      }}
-    >
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md md:max-w-lg relative">
+    <div className="flex items-center justify-center min-h-screen bg-white font-spartan">
+      <div className="relative bg-white p-8 rounded-lg w-full max-w-md md:max-w-lg">
         <NavLink
           className="absolute top-4 left-4 text-gray-700 hover:text-gray-900"
-          to="/login"
+          to="/"
         >
           <AiOutlineArrowLeft size={24} />
         </NavLink>
-        <form className="space-y-6 font-spartan">
-          <h2 className="text-2xl font-bold text-slate-950 text-center font-spartan">
-            Daftar
-          </h2>
-          <div className="form-group">
-            <label
-              htmlFor="nama"
-              className="block mb-2 text-gray-700 font-semibold font-spartan"
-            >
-              Nama Lengkap
-            </label>
-            <input
-              type="text"
-              id="nama"
-              placeholder="Masukkan Nama Lengkap Anda"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 font-spartan"
-            />
-          </div>
+        <div className="text-center mb-8">
+          <h2 className="text-2xl font-bold text-slate-950">Register</h2>
+          <p className="text-gray-700">Create a new account</p>
+        </div>
+        <form className="space-y-6">
           <div className="form-group">
             <label
               htmlFor="email"
-              className="block mb-2 text-gray-700 font-semibold font-spartan"
+              className="block mb-2 text-gray-700 font-semibold"
             >
               Email
             </label>
             <input
               type="email"
               id="email"
-              placeholder="Masukkan Email Anda"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 font-spartan"
+              placeholder="Email"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div className="form-group">
             <label
               htmlFor="password"
-              className="block mb-2 text-gray-700 font-semibold font-spartan"
+              className="block mb-2 text-gray-700 font-semibold"
             >
               Password
             </label>
             <input
               type="password"
               id="password"
-              placeholder="Masukkan Password Anda"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 font-spartan"
+              placeholder="Password"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="form-group">
+            <label
+              htmlFor="confirm-password"
+              className="block mb-2 text-gray-700 font-semibold"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              placeholder="Confirm Password"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 bg-yellow-400 text-white rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-orange-600 font-semibold font-spartan"
+            className="w-full py-2 bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold"
           >
-            Daftar
+            Register
           </button>
-          <p className="text-center text-gray-700 mt-4 font-spartan">
-            Atau daftar menggunakan akun Google
+          <p className="text-center text-gray-700 mt-4">
+            Already have an account?{" "}
+            <NavLink to="/login" className="text-blue-500">
+              Login
+            </NavLink>
           </p>
-          <a
-            className="block w-full py-2 text-center bg-yellow-400 text-white rounded-md hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-orange-600 mt-2 font-spartan"
-            href="#"
-          >
-            Daftar dengan Google
-          </a>
         </form>
       </div>
     </div>
