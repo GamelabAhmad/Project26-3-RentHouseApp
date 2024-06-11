9daed404a4822e139b244fb0de53a114bde496c1
Fian222 <fianakhmat@gmail.com>
2024-06-11 22:59:40 +0700

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,33 +1,30 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { AiOutlineArrowLeft } from "react-icons/ai";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = (e) => {
     e.preventDefault();
 
     // Contoh: cek apakah email dan password yang dimasukkan adalah "admin"
-    if (email === "admin" && password === "admin") {
+    if (email === 'admin' && password === 'admin') {
       // Jika login berhasil, tampilkan pesan toast "Login berhasil"
-      toast.success("Login berhasil");
+      toast.success('Login berhasil');
     } else {
       // Jika login gagal, tampilkan pesan toast "Login gagal"
-      toast.error("Login gagal");
+      toast.error('Login gagal');
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white font-spartan">
       <div className="relative bg-white p-8 rounded-lg w-full max-w-md md:max-w-lg">
-        <NavLink
-          className="absolute top-4 left-4 text-gray-700 hover:text-gray-900"
-          to="/"
-        >
+        <NavLink className="absolute top-4 left-4 text-gray-700 hover:text-gray-900" to="/">
           <AiOutlineArrowLeft size={24} />
         </NavLink>
         <div className="text-center mb-8">
@@ -36,26 +33,13 @@ export default function Login() {
         </div>
         <form className="space-y-6" onSubmit={handleLogin}>
           <div className="form-group">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-gray-700 font-semibold ml-1"
-            >
+            <label htmlFor="email" className="block mb-2 text-gray-700 font-semibold ml-1">
               Email
             </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-left"
-            />
+            <input type="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-left" />
           </div>
           <div className="form-group">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-gray-700 font-semibold ml-1"
-            >
+            <label htmlFor="password" className="block mb-2 text-gray-700 font-semibold ml-1">
               Password
             </label>
             <input
@@ -67,14 +51,11 @@ export default function Login() {
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-left"
             />
           </div>
-          <button
-            type="submit"
-            className="w-full py-2 bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold"
-          >
+          <button type="submit" className="w-full py-2 bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold">
             Login
           </button>
           <p className="text-center text-gray-700 mt-4">
-            Don't have an account?{" "}
+            Don't have an account?{' '}
             <NavLink to="/register" className="text-blue-500">
               Register
             </NavLink>
