19d2ff5a48c80811318d93a3a0e7374f6b13ab29
GamelabAhmad <164120476+GamelabAhmad@users.noreply.github.com>
2024-06-14 16:31:47 +0700

    Update database.js

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize('4WedGbSF4zKSpri5', 'rVdcLCqawtrnkFyB', 'LERKlH5JNVBCI4YH', {
-  host: '	educalab.id',
+  host: 'educalab.id',
   port: 3307,
   dialect: 'mysql',
   timezone: '+07:00',
