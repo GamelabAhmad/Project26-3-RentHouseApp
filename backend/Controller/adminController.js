4208e363ebb29794cf9f38338fecc55f115e78ad
Fian222 <fianakhmat@gmail.com>
2024-06-14 18:29:25 +0700

    Memperbaiki adminController

--- backend/Controller/adminController.js
+++ backend/Controller/adminController.js
@@ -1,12 +1,20 @@
 const { User } = require('../Models/userModel');
 const { Rumah } = require('../Models/rumahModel');
-const { Transaksi } = require('../Models/transaksiModel');
+const Transaksi = require('../Models/transaksiModel');
 
 const getAllUser = async (req, res) => {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+    if (users.length === 0) {
+      return res.status(404).json({ message: 'User Belum Ada' });
+    }
     res.status(200).json(users);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
@@ -14,8 +22,12 @@ const getAllUser = async (req, res) => {
 const getAllRumah = async (req, res) => {
   try {
     const rumah = await Rumah.findAll();
+    if (rumah.length === 0) {
+      return res.status(404).json({ message: 'Rumah Belum Ada' });
+    }
     res.status(200).json(rumah);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
@@ -23,8 +35,12 @@ const getAllRumah = async (req, res) => {
 const getAllTransaksi = async (req, res) => {
   try {
     const transaksi = await Transaksi.findAll();
+    if (transaksi.length === 0) {
+      return res.status(404).json({ message: 'Transaksi belum ada' });
+    }
     res.status(200).json(transaksi);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
