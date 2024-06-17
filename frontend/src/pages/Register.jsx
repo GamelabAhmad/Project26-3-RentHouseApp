ba1c3e84d01321e121eb0657fe718e8348b5c055
Fian222 <fianakhmat@gmail.com>
2024-06-17 18:16:39 +0700

    Mengubah fetch api

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
-import {
-  AiOutlineArrowLeft,
-  AiOutlineEye,
-  AiOutlineEyeInvisible,
-} from "react-icons/ai";
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [fullname, setFullname] = useState("");
-  const [nomorTelp, setNomorTelp] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [role, setRole] = useState("penyewa");
-  const [bankName, setBankName] = useState("");
-  const [accountNumber, setAccountNumber] = useState("");
-  const [error, setError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+  const [nomorTelp, setNomorTelp] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [role, setRole] = useState('penyewa');
+  const [bankName, setBankName] = useState('');
+  const [accountNumber, setAccountNumber] = useState('');
+  const [error, setError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const [showPassword, setShowPassword] = useState(false); // State untuk menampilkan atau menyembunyikan password
   const [showConfirmPassword, setShowConfirmPassword] = useState(false); // State untuk menampilkan atau menyembunyikan confirm password
   const navigate = useNavigate();
@@ -30,10 +26,10 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch("http://localhost:4000/api/users/register", {
-        method: "POST",
+      const res = await fetch('https://api-msib-6-rent-house-03.educalab.id/api/users/register', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           email,
@@ -41,18 +37,18 @@ export default function Register() {
           fullname,
           nomor_telp: nomorTelp,
           role,
-          nama_bank: role === "pemilik" ? bankName : null,
-          nomor_rekening: role === "pemilik" ? accountNumber : null,
+          nama_bank: role === 'pemilik' ? bankName : null,
+          nomor_rekening: role === 'pemilik' ? accountNumber : null,
         }),
       });
       if (res.ok) {
-        setSuccessMessage("Registration Successful");
+        setSuccessMessage('Registration Successful');
         setTimeout(() => {
-          navigate("/login");
+          navigate('/login');
         }, 1000);
       } else {
         const data = await res.json();
-        setError(data.message || "Failed to register");
+        setError(data.message || 'Failed to register');
       }
     } catch (error) {
       setError(`register failed: ${error.message}`);
@@ -62,10 +58,7 @@ export default function Register() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white font-spartan">
       <div className="relative bg-white p-8 rounded-lg w-full max-w-md md:max-w-lg">
-        <NavLink
-          className="absolute top-10 left-6 text-gray-700 hover:text-gray-900"
-          to="/"
-        >
+        <NavLink className="absolute top-10 left-6 text-gray-700 hover:text-gray-900" to="/">
           <AiOutlineArrowLeft size={24} />
         </NavLink>
         <div className="text-center mb-8">
@@ -73,43 +66,19 @@ export default function Register() {
           <p className="text-gray-700">Create a new account</p>
         </div>
         <form className="space-y-4" onSubmit={handleRegister}>
-          {error && (
-            <p className="text-red-500 text-lg font-bold uppercase">{error}</p>
-          )}
-          {successMessage && (
-            <p className="text-green-500 text-lg font-bold uppercase">
-              {successMessage}
-            </p>
-          )}
+          {error && <p className="text-red-500 text-lg font-bold uppercase">{error}</p>}
+          {successMessage && <p className="text-green-500 text-lg font-bold uppercase">{successMessage}</p>}
           <div className="form-group">
-            <label
-              htmlFor="fullname"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="fullname" className="block mb-1 text-gray-700 font-semibold ml-2">
               Fullname
             </label>
-            <input
-              onChange={(e) => setFullname(e.target.value)}
-              value={fullname}
-              type="text"
-              id="fullname"
-              placeholder="fullname"
-              className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <input onChange={(e) => setFullname(e.target.value)} value={fullname} type="text" id="fullname" placeholder="fullname" className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
           <div className="form-group">
-            <label
-              htmlFor="role"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="role" className="block mb-1 text-gray-700 font-semibold ml-2">
               Role
             </label>
-            <select
-              id="role"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              className="w-full px-1 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
+            <select id="role" value={role} onChange={(e) => setRole(e.target.value)} className="w-full px-1 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
               <option value="penyewa" className="p-4">
                 Penyewa
               </option>
@@ -118,21 +87,13 @@ export default function Register() {
               </option>
             </select>
           </div>
-          {role === "pemilik" && (
+          {role === 'pemilik' && (
             <>
               <div className="form-group">
-                <label
-                  htmlFor="bank-name"
-                  className="block mb-1 text-gray-700 font-semibold ml-2"
-                >
+                <label htmlFor="bank-name" className="block mb-1 text-gray-700 font-semibold ml-2">
                   Nama Bank
                 </label>
-                <select
-                  id="bank-name"
-                  value={bankName}
-                  onChange={(e) => setBankName(e.target.value)}
-                  className="w-full px-1 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
+                <select id="bank-name" value={bankName} onChange={(e) => setBankName(e.target.value)} className="w-full px-1 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                   <option>Pilih Bank</option>
                   <option value="BCA">BCA</option>
                   <option value="Mandiri">Mandiri</option>
@@ -142,10 +103,7 @@ export default function Register() {
                 </select>
               </div>
               <div className="form-group">
-                <label
-                  htmlFor="nomor-rekening"
-                  className="block mb-1 text-gray-700 font-semibold ml-2"
-                >
+                <label htmlFor="nomor-rekening" className="block mb-1 text-gray-700 font-semibold ml-2">
                   Nomor Rekening <span className="text-red-500 text-lg">*</span>
                 </label>
                 <input
@@ -160,80 +118,44 @@ export default function Register() {
             </>
           )}
           <div className="form-group">
-            <label
-              htmlFor="email"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="email" className="block mb-1 text-gray-700 font-semibold ml-2">
               Email
             </label>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              type="email"
-              id="email"
-              placeholder="Email"
-              className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" placeholder="Email" className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
           <div className="form-group">
-            <label
-              htmlFor="nomor_telp"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="nomor_telp" className="block mb-1 text-gray-700 font-semibold ml-2">
               Phone Number
             </label>
-            <input
-              onChange={(e) => setNomorTelp(e.target.value)}
-              value={nomorTelp}
-              type="text"
-              id="nomor_telp"
-              placeholder="089123450000"
-              className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <input onChange={(e) => setNomorTelp(e.target.value)} value={nomorTelp} type="text" id="nomor_telp" placeholder="089123450000" className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
           <div className="form-group">
-            <label
-              htmlFor="password"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="password" className="block mb-1 text-gray-700 font-semibold ml-2">
               Password
             </label>
             <div className="relative">
               <input
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
-                type={showPassword ? "text" : "password"}
+                type={showPassword ? 'text' : 'password'}
                 id="password"
                 placeholder="Password"
                 className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <button
-                type="button"
-                className="absolute top-2 right-2"
-                onMouseDown={() => setShowPassword(true)}
-                onMouseUp={() => setShowPassword(false)}
-                onMouseLeave={() => setShowPassword(false)}
-              >
-                {showPassword ? (
-                  <AiOutlineEyeInvisible size={20} />
-                ) : (
-                  <AiOutlineEye size={20} />
-                )}
+              <button type="button" className="absolute top-2 right-2" onMouseDown={() => setShowPassword(true)} onMouseUp={() => setShowPassword(false)} onMouseLeave={() => setShowPassword(false)}>
+                {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
               </button>
             </div>
           </div>
           <div className="form-group">
-            <label
-              htmlFor="confirm-password"
-              className="block mb-1 text-gray-700 font-semibold ml-2"
-            >
+            <label htmlFor="confirm-password" className="block mb-1 text-gray-700 font-semibold ml-2">
               Confirm Password
             </label>
             <div className="relative">
               <input
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 value={confirmPassword}
-                type={showConfirmPassword ? "text" : "password"}
+                type={showConfirmPassword ? 'text' : 'password'}
                 id="confirm-password"
                 placeholder="Confirm Password"
                 className="w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -246,25 +168,16 @@ export default function Register() {
                 onMouseLeave={() => setShowConfirmPassword(false)}
                 className="absolute inset-y-0 right-0 flex items-center px-2 py-1"
               >
-                {showConfirmPassword ? (
-                  <AiOutlineEyeInvisible size={20} />
-                ) : (
-                  <AiOutlineEye size={20} />
-                )}
+                {showConfirmPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
               </button>
             </div>
-            {passwordError && (
-              <p className="text-red-500 text-sm">{passwordError}</p>
-            )}
+            {passwordError && <p className="text-red-500 text-sm">{passwordError}</p>}
           </div>
-          <button
-            type="submit"
-            className="w-full py-2  bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold"
-          >
+          <button type="submit" className="w-full py-2  bg-forest text-white rounded-md hover:bg-[#1A4750] focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold">
             Register
           </button>
           <p className="text-center text-gray-700 mt-4">
-            Already have an account?{" "}
+            Already have an account?{' '}
             <NavLink to="/login" className="text-blue-500">
               Login
             </NavLink>
